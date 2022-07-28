32e59d31a2fcc8c26bf6a128b0c0073b9a7d6fe6
craig doyle <32498304+Das-Horn@users.noreply.github.com>
2022-07-28 12:28:47 +0100

    Comms with Server

--- Micro/ESP32/ESP32.ino
+++ Micro/ESP32/ESP32.ino
@@ -6,13 +6,14 @@
 
 #include <WiFi.h>
 
-#define DHTPIN 2  
+#define DHTPIN 2      /* pin number */
 #define DHTTYPE    DHT11 
 
 const char* ssid     = "your-ssid";
 const char* password = "your-password";
 
 const char* host = "your url";
+const int hostport = 0000;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -43,6 +44,7 @@ void loop() {
   else {
     Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
+    send_string_to_server(event.temperature);
     Serial.println(F("°C"));
   }
   // Get humidity event and print its value.
@@ -54,5 +56,22 @@ void loop() {
     Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
+  }
+}
+
+
+//This is the function that sends the data to the server.
+void send_string_to_server(float data) {
+  WiFiClient client;
+
+
+  if(WiFi.status() == WL_CONNECTED){
+    if(!client.connect(host, hostport)){
+      Serial.println("Error connecting to server!");
+      return;
+    }
+
+    client.print(String(data));
+    client.stop();
   }
 }
